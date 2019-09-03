483d64787144f8b11d46a41c3b0ac37932706938
Vijith Assar <github@vijithassar.com>
2019-09-02 21:58:13 -0400

    development environment

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+const config = {
+    input: "./pingkiller.js",
+    output: {
+      file: "./pingkiller.build.js",
+      format: "iife",
+      sourcemap: true
+    }
+  }
+  
+  export default config
